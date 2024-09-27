3004dfc737e7ca474b6defc23b2bd9bb9af65b72
Elizabeth Snyman <79578984+SnymanE@users.noreply.github.com>
2024-09-27 08:49:54 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -21,3 +21,23 @@ var siteConfig = {
     excludedFilters: ['publishingCountryCode'], // it makes no sense to show this filter as we scope the data by a single value
   }
 };
+literature: {
+    rootFilter: {
+      predicate: {
+        type: 'or', predicates: [
+          {
+            type: 'in',
+            key: 'countriesOfResearcher',
+            values: ['ZA']
+          },
+          {
+            type: 'in',
+            key: 'countriesOfCoverage',
+            values: ['ZA']
+          }
+        ]
+      }
+    },
+    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+  }
+};
